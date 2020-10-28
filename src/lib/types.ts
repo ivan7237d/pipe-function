9bd88962f883b78ffe41827172ffaa2f025ef605
Ivan Novikov <ivan7237d@gmail.com>
2020-10-28 17:33:57 -0400

    feat: reducer type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,2 +1,6 @@
 export type UnaryFunction<From, To> = (source: From) => To;
 export type MonoTypeUnaryFunction<T> = UnaryFunction<T, T>;
+export type Reducer<Accumulator, Element> = (
+  accumulator: Accumulator,
+  element: Element,
+) => Accumulator;
