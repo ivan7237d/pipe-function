c802a59048f8b60be3d41d799661b97c3ce2b9ca
Ivan Novikov <ivan7237d@gmail.com>
2020-10-28 17:47:17 -0400

    chore: remove unneeded types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,3 @@
-export type UnaryFunction<From, To> = (source: From) => To;
-export type MonoTypeUnaryFunction<T> = UnaryFunction<T, T>;
 export type Reducer<Accumulator, Element> = (
   accumulator: Accumulator,
   element: Element,
