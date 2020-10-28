11c5c877abbc009ab2a18691c553f9d66ba34ba8
Ivan Novikov <ivan7237d@gmail.com>
2020-10-28 19:58:07 -0400

    fix(iterable): add exports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 export { applyPipe } from './lib/applyPipe';
+export { anyIterable } from './lib/iterable/anyIterable';
+export { concatIterable } from './lib/iterable/concatIterable';
+export { everyIterable } from './lib/iterable/everyIterable';
+export { filterIterable } from './lib/iterable/filterIterable';
+export { firstIterable } from './lib/iterable/firstIterable';
+export { flatMapIterable } from './lib/iterable/flatMapIterable';
+export { forEachIterable } from './lib/iterable/forEachIterable';
+export { isEmptyIterable } from './lib/iterable/isEmptyIterable';
+export { isFirstIterable } from './lib/iterable/isFirstIterable';
+export { mapIterable } from './lib/iterable/mapIterable';
+export { rangeIterable } from './lib/iterable/rangeIterable';
+export { reduceIterable } from './lib/iterable/reduceIterable';
+export { scanIterable } from './lib/iterable/scanIterable';
+export { takeIterable } from './lib/iterable/takeIterable';
+export { takeWhileIterable } from './lib/iterable/takeWhileIterable';
+export { zipIterable } from './lib/iterable/zipIterable';
