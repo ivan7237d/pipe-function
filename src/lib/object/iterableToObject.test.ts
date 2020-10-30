de85ff11a4bc86ae8d15e8abbb82440823cc10dd
Ivan Novikov <ivan7237d@gmail.com>
2020-10-30 17:58:06 -0400

    refactor(object): simplify code

--- src/lib/object/iterableToObject.test.ts
+++ src/lib/object/iterableToObject.test.ts
@@ -1,16 +1,14 @@
 import { applyPipe } from '../applyPipe';
-import { identity } from '../identity';
 import { iterableToObject } from './iterableToObject';
 
 it('works', () => {
   expect(
     applyPipe(
-      identity<[string, number | undefined][]>([
+      iterableToObject([
         ['a', 0],
         ['b', 1],
         ['c', undefined],
       ]),
-      iterableToObject,
     ),
   ).toMatchInlineSnapshot(`
     Object {
