47527cf67a46b0482bf10a94b93fe5c1c55f7a38
Ivan Novikov <ivan7237d@gmail.com>
2020-11-19 18:42:14 -0500

    test(iterable): add two more tests for rangeIterable

--- src/lib/iterable/rangeIterable.test.ts
+++ src/lib/iterable/rangeIterable.test.ts
@@ -11,6 +11,9 @@ it('works', () => {
       2,
     ]
   `);
+  expect(
+    applyPipe(rangeIterable(undefined, 0), (source) => [...source]),
+  ).toMatchInlineSnapshot(`Array []`);
   expect(applyPipe(rangeIterable(undefined, 3), (source) => [...source]))
     .toMatchInlineSnapshot(`
     Array [
@@ -26,6 +29,9 @@ it('works', () => {
       2,
     ]
   `);
+  expect(
+    applyPipe(rangeIterable(0, 0), (source) => [...source]),
+  ).toMatchInlineSnapshot(`Array []`);
   expect(applyPipe(rangeIterable(10, 30, 10), (source) => [...source]))
     .toMatchInlineSnapshot(`
     Array [
