106569d2f4277ebf4387919cabf64e5eb5823523
Ivan Novikov <ivan7237d@gmail.com>
2020-12-09 18:46:47 -0500

    feat(lastInIterable): better performance for arrays

--- src/internal/iterable/lastInIterable.test.ts
+++ src/internal/iterable/lastInIterable.test.ts
@@ -1,7 +1,22 @@
 import { applyPipe } from '../applyPipe';
 import { lastInIterable } from './lastInIterable';
+import { mapIterable } from './mapIterable';
 
 it('works', () => {
   expect(applyPipe([0, 1], lastInIterable)).toEqual(1);
   expect(applyPipe([], lastInIterable)).toEqual(undefined);
+  expect(
+    applyPipe(
+      [0, 1],
+      mapIterable((value) => value),
+      lastInIterable,
+    ),
+  ).toEqual(1);
+  expect(
+    applyPipe(
+      [],
+      mapIterable((value) => value),
+      lastInIterable,
+    ),
+  ).toEqual(undefined);
 });
--- src/internal/iterable/lastInIterable.ts
+++ src/internal/iterable/lastInIterable.ts
@@ -1,7 +1,12 @@
 /**
- * Returns the last value, or `undefined` if the iterable is empty.
+ * Returns the last value, or `undefined` if the iterable is empty. If
+ * the iterable is an array, retrieves the last value by index, otherwise
+ * iterates to the end.
  */
 export const lastInIterable = <T>(iterable: Iterable<T>): T | undefined => {
+  if (Array.isArray(iterable)) {
+    return iterable[iterable.length - 1];
+  }
   let result: T | undefined = undefined;
   // eslint-disable-next-line no-empty
   for (result of iterable) {
