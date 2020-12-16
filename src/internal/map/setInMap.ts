2ab1b0c35a28aec08f4f9588097c0d3de80c6ef9
Ivan Novikov <ivan7237d@gmail.com>
2020-12-16 11:12:04 -0500

    refactor(setInMap): rename generic type

--- src/internal/map/setInMap.ts
+++ src/internal/map/setInMap.ts
@@ -2,9 +2,9 @@
  * Diverges from the native method in that if the provided value is `undefined`,
  * the key is deleted from the map.
  */
-export const setInMap = <Key, Value, ValueArg extends Value>(
+export const setInMap = <Key, Value, NewValue extends Value>(
   key: Key,
-  value: ValueArg | undefined,
+  value: NewValue | undefined,
 ) => (map: ReadonlyMap<Key, Value>): Map<Key, Value> => {
   const copy = new Map(map);
   if (value === undefined) {
